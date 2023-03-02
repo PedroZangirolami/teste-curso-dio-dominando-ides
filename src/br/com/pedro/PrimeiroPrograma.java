f19927ff37471155265cbed2c1273186c20aed12
PedroZangirolami <pedrozangirolami@gmail.com>
2023-03-02 17:30:38 -0300

    first commit

---
+++ src/br/com/pedro/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package br.com.pedro;
+
+import br.com.pedro.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+	
+		Gato gato = new Gato();
+		Livro livro = new Livro();
+
+		System.out.println(gato);
+		System.out.println(livro);
+	}
+
+}
+
+ class Livro {
+	
+}
